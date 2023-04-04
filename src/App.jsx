c07c559b05f72bf6348cfdb790be13cee88a9709
Aizadasyrdybaeva <aizadasayakbaevna15@gmail.com>
2023-04-04 16:04:27 +0600

    added NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { NavLink } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      asdfff
+      <h1>Hello world</h1>
+      <p>This is my diploma</p>
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">About</NavLink>
+      </nav>
+
+
     </div>
-  )
+  );
 }
 
 export default App
