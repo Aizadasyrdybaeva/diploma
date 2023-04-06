8d89fbf2e99973fad2807ffead2904211be42fcc
Aizadasyrdybaeva <aizadasayakbaevna15@gmail.com>
2023-04-06 15:06:09 +0600

    added Contact and Delivery

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,6 +7,7 @@
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/delivery">Delivery</NavLink>
         
       </nav>
 
