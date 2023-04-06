7d0dc80e8c03bbf35c63291961f3f5e425570906
Aizadasyrdybaeva <aizadasayakbaevna15@gmail.com>
2023-04-06 14:56:50 +0600

    added Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
+import Layout from './components/Layout/Layout';
+import { Route, Routes } from 'react-router-dom';
 
-import { NavLink, Route, Routes } from 'react-router-dom';
-import './App.css'
 
-function App() {
+export default function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <p>This is my diploma</p>
-
-    {/* создаем навигацию */}
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        
-      </nav>
-
-     {/* //создаем путь */}
-      <main>
-        <Routes>
+       <Layout>
+       <Routes>
           <Route path="/" element={<h2>Home</h2>} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contact" element={<h2>Contact</h2>} />
     
         </Routes>
-      </main>
+       </Layout>
+      
 
-
-    </div>
+</div>
   );
 }
 
-export default App
+
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+ import "./Nav.css";
+ import { NavLink } from "react-router-dom";
+
+ export default function Nav(){
+  return (
+    <nav className="Nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        
+      </nav>
+
+  );
+ }
