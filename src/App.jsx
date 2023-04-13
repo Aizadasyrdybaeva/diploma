29b91e08b5ef2cbabcc94f496c05b11f37e1040f
Aizadasyrdybaeva <aizadasayakbaevna15@gmail.com>
2023-04-13 14:46:02 +0600

    Move database fetch to App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,53 @@ import Delivery from "./pages/Delivery";
 import Wholesale from "./pages/Wholesale";
 import Product from "./pages/Product";
 import Category from "./pages/Category";
+import { categoryCollection } from "./firebase";
+import { getDocs } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+
+export const AppContext = createContext({
+  categories: [],
+});
 
 export default function App() {
+  const [categories, setCategories] = useState([]);
+  //выполнить эту функцию только один раз
+
+  useEffect(() => {
+    // получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      //категории будут храниться в snapshot.docs
+
+      //создать массив для категорий
+      const newCategories = [];
+      //заполнить массив данными из списка категорий
+      snapshot.docs.forEach((doc) => {
+        //doc = категория
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+      //задать новый массив как состояние компонента
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/wholesale" element={<Wholesale />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/category/:path" element={<Category />} />
-        </Routes>
-      </Layout>
+      <AppContext.Provider value={{ categories }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/wholesale" element={<Wholesale />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/category/:path" element={<Category />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,32 +1,15 @@
 import { getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 import { categoryCollection } from "../../firebase";
 import "./CategoryList.css";
 
 export default function CategoryList() {
-  const [categories, setCategories] = useState([]);
-  //выполнить эту функцию только один раз
-  useEffect(() => {
-    // получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      //категории будут храниться в snapshot.docs
+  const { categories } = useContext(AppContext);
 
-      //создать массив для категорий
-      const newCategories = [];
-      //заполнить массив данными из списка категорий
-      snapshot.docs.forEach((doc) => {
-        //doc = категория
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      //задать новый массив как состояние компонента
-      setCategories(newCategories);
-    });
-  }, []);
-
+  
+  
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
