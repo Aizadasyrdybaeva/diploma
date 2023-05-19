eaf880f3adf93b044c57100949c504cd85478329
Aizadasyrdybaeva <aizadasayakbaevna15@gmail.com>
2023-05-19 15:32:13 +0600

    add video in Home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,26 @@
-export default function Home(){
-  return(
+import video1 from "../assets/shariki.mp4";
+
+export default function Home() {
+  return (
     <div className="Home">
       <h1>Welcome to our "For You" shop!</h1>
+
+      <iframe
+        width="500"
+        height="300"
+        src={video1}
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        
+      ></iframe>
+
       <p>
         Friends, are you planning to make a purchase with maximum comfort,
         minimum risks and a favorable price? You were in the right place, at the
         right time. Welcome to us - to the world of online store "For you"!
         Today it is 50.000 products in 400 product categories
       </p>
-      <video>
-  <source src="" />
-</video>
-     
-      
-
-      
     </div>
   );
 }
